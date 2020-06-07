27a82d5f9d61403ab9b71f6d67f1d46a8bf5b2eb
s244926 <s244926@studenti.polito.it>
2020-06-08 00:15:58 +0200

    Progetto Base

---
+++ src/main/java/it/polito/tdp/borders/FXMLController.java
@@ -0,0 +1,53 @@
+package it.polito.tdp.borders;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.borders.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtAnno;
+
+    @FXML
+    private ComboBox<?> boxNazione;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doCalcolaConfini(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doSimula(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Borders.fxml'.";
+        assert boxNazione != null : "fx:id=\"boxNazione\" was not injected: check your FXML file 'Borders.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Borders.fxml'.";
+
+    }
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+}
---
+++ src/main/java/it/polito/tdp/borders/db/TestDAO.java
@@ -0,0 +1,18 @@
+package it.polito.tdp.borders.db;
+
+import java.util.List;
+
+import it.polito.tdp.borders.model.Country;
+
+public class TestDAO {
+
+	public static void main(String[] args) {
+		List<Country> list ;
+		BordersDAO dao = new BordersDAO() ;
+		list = dao.loadAllCountries() ;
+		for(Country c: list) {
+			System.out.println(c);
+		}
+	}
+
+}
